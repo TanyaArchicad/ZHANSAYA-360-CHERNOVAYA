bb53c37dc18a103fa93fc60e900b344f2d815e95
TanyaArchicad <tanyadiz2014@gmail.com>
2025-04-18 13:49:19 +0500

    Upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--01-",
+      "name": "Панорама 01 Черновая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6639335016780148,
+        "pitch": 0.04949119821550596,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7291238324396545,
+          "pitch": 0.0417904532399298,
+          "rotation": 0,
+          "target": "1--02-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--02-",
+      "name": "Панорама 02 Черновая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6784046996388753,
+        "pitch": 0.0715804730753895,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.31942376842259,
+          "pitch": 0.028910945167760715,
+          "rotation": 0,
+          "target": "0--01-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
